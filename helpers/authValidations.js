24725652b4750fce1a4c0c075ec5252aa3157a28
Henrik Hayrapetyan <henrikhayrapetyan91@gmail.com>
2022-12-26 22:23:56 +0400

    modified validation

--- helpers/authValidations.js
+++ helpers/authValidations.js
@@ -2,11 +2,11 @@ import { body } from "express-validator";
 
 export const registerValidator = [
     body('email', 'Invalid email format').isEmail(),
-    body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
-    body('fullname', 'Enter your name').isLength({ min: 3 }),
+    body('password', 'Password must be at least 5 characters').isLength({ min: 5 }).matches(/^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z0-9]{7,20}$/),
+    body('fullname', 'Enter your name').isLength({ min: 3 }).notEmpty(),
 ]
 
 export const loginValidator = [
     body('email', 'Invalid email format').isEmail(),
-    body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
+    body('password', 'Password must be at least 5 characters').isLength({ min: 5 }).matches(/^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z0-9]{7,20}$/),
 ]
